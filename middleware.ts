c4e60e05c125bdea4d33fb8e8b672e189a9a2e3e
narendraanbazhagann <delzo.admin@gmail.com>
2026-04-07 12:25:49 +0530

    feat: initial commit

---
+++ middleware.ts
@@ -0,0 +1,20 @@
+import { clerkMiddleware } from '@clerk/nextjs/server';
+
+export default clerkMiddleware(async (auth, req) => {
+  if (req.nextUrl.pathname.startsWith('/dashboard')) {
+    const { userId, redirectToSignIn } = await auth();
+
+    if (!userId) {
+      return redirectToSignIn();
+    }
+  }
+});
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+};
